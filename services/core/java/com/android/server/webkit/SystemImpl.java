f23b1e5522b066549fd495f658d5d70908fd1936
Yaroslav Zviezda <acroreiser@gmail.com>
2023-07-12 23:34:39 +0300

    Revert "Enable multiprocess WebView for all devices."
    This reverts commit 2bdf647b7d9412d7622b843234b38b7189702b35.

--- services/core/java/com/android/server/webkit/SystemImpl.java
+++ services/core/java/com/android/server/webkit/SystemImpl.java
@@ -259,8 +259,10 @@ public class SystemImpl implements SystemInterface {
 
     @Override
     public boolean isMultiProcessDefaultEnabled() {
-        // Multiprocess is enabled by default for all devices.
-        return true;
+        // Multiprocess is enabled for all 64-bit devices, since the ability to run the renderer
+        // process in 32-bit when it's a separate process typically results in a net memory saving.
+        // Multiprocess is also enabled for 32-bit devices unless they report they are "low ram".
+        return Build.SUPPORTED_64_BIT_ABIS.length > 0 || !ActivityManager.isLowRamDeviceStatic();
     }
 
     // flags declaring we want extra info from the package manager for webview providers
